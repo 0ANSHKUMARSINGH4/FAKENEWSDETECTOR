8d194582191970dcd7046d6cde70b808a3edcb5c
Ansh Kumar Singh <3000ansh2004@gmail.com>
2025-11-12 21:33:02 +0530

    Ready-to-deploy version with model included and improved accuracy

--- app/utils/predictor.py
+++ app/utils/predictor.py
@@ -1,6 +1,7 @@
 import os
 import joblib
 
+# Absolute path to the model folder
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 MODEL_PATH = os.path.join(BASE_DIR, "model", "bias_model.pkl")
 VECT_PATH = os.path.join(BASE_DIR, "model", "vectorizer.pkl")
